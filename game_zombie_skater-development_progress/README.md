0ff39671600988074d92c79c5eb8a672034862c6
yotaro-redson <yotaropn@gmail.com>
2023-07-16 22:53:33 +0630

    kbjiv
    supvh uh uhyuyyvipu

---
+++ game_zombie_skater-development_progress/README.md
@@ -0,0 +1,2 @@
+# game_zombie_skater
+We will develop a skating endless game in zombie apocalypse setting
---
+++ game_zombie_skater-development_progress/Source codes/game_prototype/endless_terrain-main/player.gd
@@ -0,0 +1,27 @@
+extends CharacterBody3D
+##
+## Simple left/right character controller
+##
+
+## Speed of character movement
+const SPEED = 5.0
+var gravity=20
+var jump_impulse=10
+
+func _physics_process(_delta: float) -> void:
+	
+	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	# We'll ignore up and down input, just using side to side
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	if direction:
+		velocity.x = direction.x * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+	var collision = get_last_slide_collision()
+	if collision:
+		print("Collided with: ", collision.get_collider())
+		get_tree().quit()
